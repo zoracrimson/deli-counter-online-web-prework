b26107a3cd72e9000282dd5a962e74103082ccbd
Kat Maldon <katharinamaldon@gmail.com>
2019-01-19 21:42:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,17 @@ def now_serving(katz_deli)
     puts "Currently serving #{katz_deli.shift}."
   end
 end
+
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else
+    puts "The line is currently:"
+    katz_deli.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value} "
+    end
+    puts "#{message}"
+  end
+end
+
+      
