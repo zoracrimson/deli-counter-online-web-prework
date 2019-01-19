ab22f9203b52a9f159f2eb5484c4aa76f4ac3d3f
Kat Maldon <katharinamaldon@gmail.com>
2019-01-19 21:38:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,3 +3,10 @@ def take_a_number(katz_deli)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
