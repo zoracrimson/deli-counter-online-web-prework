030e97e23ad1fb2448cde433291f35147cd8e603
Kat Maldon <katharinamaldon@gmail.com>
2019-01-19 21:44:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,10 @@
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  end
+end 
+    
+
 def take_a_number(katz_deli)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
